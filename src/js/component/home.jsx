7806fffb555601511de3244bffb1d6654ba0b16b
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-03-31 16:32:00 +0000

    made changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [item, setItem] = useState("");
+	const [todoList, setTodolist] = useState([]);
+	const handleRemoveItem = (e) => {
+		const name = e.target.getAttribute("name");
+		updateList(list.filter((item) => item.name !== name));
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="input-group mb-3">
+			{todoList.map((item) => {
+				return (
+					<>
+						<span
+							name={item.name}
+							onClick={handleRemoveItem}></span>
+						<span key={index}>{item.name}</span>
+					</>
+				);
+			})}
+			<input
+				type="test"
+				className="form-control"
+				placeholder="add a task"
+				onChange={(e) => setItem(e.target.value)}
+				value={item}
+			/>
+			<button
+				onClick={() => {
+					setTodolist([...todoList, item]);
+					console.log(todoList);
+				}}
+				type="btn btn-secondary"
+				className="input-group-test"
+				id="basic-addon1">
+				Add Task
+			</button>
+			<ul>
+				{todoList.map((element, index) => (
+					<li key={index}>{element}</li>
+				))}
+			</ul>
 		</div>
 	);
 };
