36827a752a74edaae6e8df2f695fa45de391d715
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-03-31 19:25:46 +0000

    made changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,3 +1,4 @@
+// import { element } from "prop-types";
 import React, { useState } from "react";
 
 //include images into your bundle
@@ -8,44 +9,40 @@ const Home = () => {
 	const [item, setItem] = useState("");
 	const [todoList, setTodolist] = useState([]);
 	const handleRemoveItem = (e) => {
-		const name = e.target.getAttribute("name");
-		updateList(list.filter((item) => item.name !== name));
+		// const name = e.target.getAttribute("name")
+		setTodolist(todoList.filter((element) => element !== element));
+		console.log("clicked");
 	};
 	return (
-		<div className="input-group mb-3">
-			{todoList.map((item) => {
-				return (
-					<>
-						<span
-							name={item.name}
-							onClick={handleRemoveItem}></span>
-						<span key={index}>{item.name}</span>
-					</>
-				);
-			})}
-			<input
-				type="test"
-				className="form-control"
-				placeholder="add a task"
-				onChange={(e) => setItem(e.target.value)}
-				value={item}
-			/>
-			<button
-				onClick={() => {
-					setTodolist([...todoList, item]);
-					console.log(todoList);
-				}}
-				type="btn btn-secondary"
-				className="input-group-test"
-				id="basic-addon1">
-				Add Task
-			</button>
+		<>
+			<div className="input-group mb-3">
+				<input
+					type="test"
+					className="form-control"
+					placeholder="add a task"
+					onChange={(e) => setItem(e.target.value)}
+					value={item}
+				/>
+				<button
+					onClick={() => {
+						setTodolist([...todoList, item]);
+						console.log(todoList);
+					}}
+					type="btn btn-secondary"
+					className="input-group-test"
+					id="basic-addon1">
+					Add Task
+				</button>
+			</div>
 			<ul>
 				{todoList.map((element, index) => (
-					<li key={index}>{element}</li>
+					<>
+						<li key={index}>{element}</li>
+						<button onClick={handleRemoveItem}>delete</button>
+					</>
 				))}
 			</ul>
-		</div>
+		</>
 	);
 };
 
